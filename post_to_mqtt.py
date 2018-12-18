86b36efe6912ac07985d109730958d5b232b0ef4
Tuure Laurinolli <tuure@laurinolli.net>
2018-12-18 21:40:20 +0000

    Send test data when 'test' is present on command line

--- post_to_mqtt.py
+++ post_to_mqtt.py
@@ -36,12 +36,15 @@ def start_listen_ble():
     except OSError:
         print("Error listening for BLE advertisements, using fixed test data instead")
 
-        data = """{"svc_data_uuid16": null, "uri": null, "type": "ADV_IND", "adv_itvl": null, "svc_data_uuid32": null, "flags": 26, "address_type": "PUBLIC", "appearance": null, "svc_data_uuid128": null, "rssi": -53, "raw_data": null, "tx_pwr_lvl": null, "uuid128s": [{"_uuid_obj": {"int": 270829573007142736156654800289003511292}, "_uuid": "cbbfe0e1-f7f3-4206-84e0-84cbb3d09dfc"}], "uuid16s": [], "_name": null, "public_tgt_addr": null, "mfg_data": null, "address": {"address": "88:82:12:99:0F:D8"}, "name_is_complete": false, "uuid32s": [], "service_data": null}"""
-        async def generate_test_data():
-            while True:
-                await send_queue.put(data)
-                await asyncio.sleep(1)
-        asyncio.ensure_future(generate_test_data())
+        start_send_test_data()
+
+def start_send_test_data():
+    data = """{"svc_data_uuid16": null, "uri": null, "type": "ADV_IND", "adv_itvl": null, "svc_data_uuid32": null, "flags": 26, "address_type": "PUBLIC", "appearance": null, "svc_data_uuid128": null, "rssi": -53, "raw_data": null, "tx_pwr_lvl": null, "uuid128s": [{"_uuid_obj": {"int": 270829573007142736156654800289003511292}, "_uuid": "cbbfe0e1-f7f3-4206-84e0-84cbb3d09dfc"}], "uuid16s": [], "_name": null, "public_tgt_addr": null, "mfg_data": null, "address": {"address": "88:82:12:99:0F:D8"}, "name_is_complete": false, "uuid32s": [], "service_data": null}"""
+    async def generate_test_data():
+        while True:
+            await send_queue.put(data)
+            await asyncio.sleep(1)
+    asyncio.ensure_future(generate_test_data())
 
 
 async def post_data():
@@ -58,7 +61,10 @@ async def main():
     try:
         await client.connect(mqtt_url)
         print("Connected")
-        start_listen_ble()
+        if not 'test' in sys.argv:
+            start_listen_ble()
+        else:
+            start_send_test_data()
     except Exception as e:
         print("Connection failed: %s" % e)
         asyncio.get_event_loop().stop()
